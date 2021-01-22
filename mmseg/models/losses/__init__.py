3ebe6ab5be73e0c12e1e983121b3e1e519173097
JZTgentle <48715040+JZTgentle@users.noreply.github.com>
2021-01-22 17:25:24 +0800

    Update __init__.py

--- mmseg/models/losses/__init__.py
+++ mmseg/models/losses/__init__.py
@@ -3,9 +3,11 @@ from .cross_entropy_loss import (CrossEntropyLoss, binary_cross_entropy,
                                  cross_entropy, mask_cross_entropy)
 from .lovasz_loss import LovaszLoss
 from .utils import reduce_loss, weight_reduce_loss, weighted_loss
+from .bce_loss import BCELoss
 
 __all__ = [
     'accuracy', 'Accuracy', 'cross_entropy', 'binary_cross_entropy',
     'mask_cross_entropy', 'CrossEntropyLoss', 'reduce_loss',
-    'weight_reduce_loss', 'weighted_loss', 'LovaszLoss'
+    'weight_reduce_loss', 'weighted_loss', 'LovaszLoss',
+    'BCELoss',
 ]
