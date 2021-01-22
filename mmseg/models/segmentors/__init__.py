d25363cc5b1269a4bcd243957b901ebb862bf32d
JZTgentle <48715040+JZTgentle@users.noreply.github.com>
2021-01-22 17:29:34 +0800

    Update __init__.py

--- mmseg/models/segmentors/__init__.py
+++ mmseg/models/segmentors/__init__.py
@@ -1,4 +1,5 @@
 from .cascade_encoder_decoder import CascadeEncoderDecoder
 from .encoder_decoder import EncoderDecoder
+from .sod_encoder_decoder import SODEncoderDecoder
 
-__all__ = ['EncoderDecoder', 'CascadeEncoderDecoder']
+__all__ = ['EncoderDecoder', 'CascadeEncoderDecoder', 'SODEncoderDecoder']
