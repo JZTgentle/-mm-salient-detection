129bffb478d7f9fadfa76c2ef04d2ec9198c0880
JZTgentle <48715040+JZTgentle@users.noreply.github.com>
2021-01-22 17:21:27 +0800

    Update __init__.py

--- mmseg/core/evaluation/__init__.py
+++ mmseg/core/evaluation/__init__.py
@@ -1,8 +1,10 @@
 from .class_names import get_classes, get_palette
 from .eval_hooks import DistEvalHook, EvalHook
 from .metrics import eval_metrics, mean_dice, mean_iou
+from .sod_eval import F_measure, P_R, MAE
 
 __all__ = [
     'EvalHook', 'DistEvalHook', 'mean_dice', 'mean_iou', 'eval_metrics',
-    'get_classes', 'get_palette'
+    'get_classes', 'get_palette',
+    'F_measure', 'P_R', 'MAE',
 ]
