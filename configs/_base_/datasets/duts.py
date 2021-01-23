2b74120f89f186e4854966d7196749d4384d2743
JZTgentle <48715040+JZTgentle@users.noreply.github.com>
2021-01-23 11:29:09 +0800

    Update duts.py

--- configs/_base_/datasets/duts.py
+++ configs/_base_/datasets/duts.py
@@ -7,12 +7,12 @@ crop_size = (512, 1024)
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations', reduce_one_label=True),
-    dict(type='Resize', img_scale=(1024, 512), ratio_range=(0, 0.5, 0.75, 1.0)),
-    # dict(type='RandomCrop', crop_size=crop_size, cat_max_ratio=0.9),
+    dict(type='Resize', img_scale=(1024*1.5, 512*1.5), ratio_range=(0, 0.5, 0.75, 1.0)),
+    dict(type='RandomCrop', crop_size=crop_size, cat_max_ratio=0.9),
     dict(type='RandomFlip', flip_ratio=0.5),
     dict(type='PhotoMetricDistortion'),
     dict(type='Normalize', **img_norm_cfg),
-    # dict(type='Pad', size=crop_size, pad_val=0, seg_pad_val=255),
+    dict(type='Pad', size=crop_size, pad_val=0, seg_pad_val=0),
     dict(type='DefaultFormatBundle'),
     dict(type='Collect', keys=['img', 'gt_semantic_seg']),
 ]
